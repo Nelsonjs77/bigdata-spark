76a4b60f50c4b510071a816abf8d1d32f2067e10
Nelsonjs77 <111380066+Nelsonjs77@users.noreply.github.com>
2023-02-14 21:26:19 +0000

    add parcial1 whit hello.cpp

---
+++ parcial1/suma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//print the string enclosed in double quotes
+
+  cout << "hello world" << endl;
+  cout << "hello galaxy" << endl;
+  cout << "hello universo" << endl;
+
+  return 0;
+}
