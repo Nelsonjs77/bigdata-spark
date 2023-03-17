aa707c59472de26b73425a076917b1aa7cd41e0e
Nelsonjs77 <nelsonjuarezsandoval4@gmail.com>
2023-03-17 15:38:39 -0600

    add code

---
+++ parcial1/Octave/matris_traspuesta.m
@@ -0,0 +1,13 @@
+A = [0, 1, 2;
+     3, 4, 5;
+     6, 7, 8];
+
+renglones = 3;
+columnas = 3;
+
+for ren=1:renglones;
+  for col=1:columnas;
+      fprintf('%d\t',  A(col,ren))
+  endfor
+  fprintf('\n')
+endfor
